fb5d511885578d8e39dcf205fe3e6ae8d6a7e8be
Maddock <sowtag@gmail.com>
2025-11-03 22:53:14 -0300

    feat: spring security dep for commons module

--- service-commons/build.gradle.kts
+++ service-commons/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-security")
