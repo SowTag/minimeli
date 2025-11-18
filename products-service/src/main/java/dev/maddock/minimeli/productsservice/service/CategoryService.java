d23aff1e412bf57c2efa07638e8cca8bd2a1bfc5
Maddock <sowtag@gmail.com>
2025-11-18 10:12:36 -0300

    feat: define new category service methods

--- products-service/src/main/java/dev/maddock/minimeli/productsservice/service/CategoryService.java
+++ products-service/src/main/java/dev/maddock/minimeli/productsservice/service/CategoryService.java
@@ -1,14 +1,23 @@
 package dev.maddock.minimeli.productsservice.service;
 
 import dev.maddock.minimeli.productsservice.dto.category.CategoryDto;
+import dev.maddock.minimeli.productsservice.dto.category.CategoryTreeNode;
 import dev.maddock.minimeli.productsservice.dto.category.CreateCategoryRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CategoryService {
     CategoryDto createCategory(CreateCategoryRequest request);
 
-    CategoryDto moveCategory(UUID id, UUID newParentId);
+    void deleteCategory(UUID categoryId);
 
-    CategoryDto getCategoryById(UUID id);
+    CategoryDto getCategoryById(UUID categoryId);
+
+    List<CategoryTreeNode> getAllCategoriesAsTree();
+
+    List<CategoryDto> getAllCategories();
+
+    CategoryDto moveCategory(UUID categoryId, UUID newParentId);
+
 }
