8d76acad3634f6312bc44bbbda3ece699f49e6af
Maddock <sowtag@gmail.com>
2025-11-18 13:04:03 -0300

    feat: add .from to CategoryDto

--- products-service/src/main/java/dev/maddock/minimeli/productsservice/dto/category/CategoryDto.java
+++ products-service/src/main/java/dev/maddock/minimeli/productsservice/dto/category/CategoryDto.java
@@ -1,10 +1,22 @@
 package dev.maddock.minimeli.productsservice.dto.category;
 
+import dev.maddock.minimeli.productsservice.entity.Category;
+import lombok.Builder;
+
 import java.io.Serializable;
 import java.util.UUID;
 
 /**
  * DTO for {@link dev.maddock.minimeli.productsservice.entity.Category}
  */
+@Builder
 public record CategoryDto(UUID id, String title, String description, UUID parentCategoryId) implements Serializable {
+    public static CategoryDto from(Category category) {
+        return CategoryDto.builder()
+                .id(category.getId())
+                .title(category.getTitle())
+                .description(category.getDescription())
+                .parentCategoryId(category.getParentCategory().getId())
+                .build();
+    }
 }
